ad75b51215d464220a6820f5cda46e3afc2d82ab
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-10-09 18:46:21 -0500

    embed twitch feed

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -1,13 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
+import * as ReactBootStrap from "react-bootstrap";
 
-class Watch extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Watch Online</h1>
-            </div>
-        );
-    }
+const Watch = () => {
+    return (
+        <ReactBootStrap.Container fluid
+                                  className='text-center'>
+            <ReactBootStrap.ResponsiveEmbed aspectRatio="16by9">
+                <iframe src="https://player.twitch.tv/?channel=yasminismean&parent=localhost" frameBorder="0"
+                        allowFullScreen="true" scrolling="no" height="378" width="620"/>
+            </ReactBootStrap.ResponsiveEmbed>
+        </ReactBootStrap.Container>
+    );
 }
 
 export default Watch;
