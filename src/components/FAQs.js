d167f3088a355d69ad1d13c08e18eaaee48f36ee
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-10-09 18:45:29 -0500

    coming soon

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -1,13 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
+import * as ReactBootStrap from "react-bootstrap";
 
-class FaQs extends Component {
-    render() {
-        return (
-            <div>
-                <h1>FAQs</h1>
-            </div>
-        );
-    }
+const FAQs = () => {
+    return (
+        <ReactBootStrap.Container fluid
+                                  className='text-center'
+                                  style={{backgroundColor: 'white', padding: '50px 50px'}}>
+            <h1>Coming Soon</h1>
+        </ReactBootStrap.Container>
+    );
 }
 
-export default FaQs;
+export default FAQs;
