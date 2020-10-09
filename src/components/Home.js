02ec9b436608668ea6f1f4caea448a0e67c18889
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-10-09 12:23:16 -0500

    styling in progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
+import * as ReactBootStrap from "react-bootstrap";
+import cover from '../img/cover.png'
 
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home</h1>
-                <p>Home page body content</p>
+const Home = () => {
+    return (
+        <div>
+            <div className='container py-2'>
+                <ReactBootStrap.Image src={cover} fluid/>
             </div>
-        );
-    }
+            <div className='text-center' style={{backgroundColor: 'white', padding: '50px 50px'}}>
+                <h1>Lynn Powell & Yasmin Menchaca</h1>
+            </div>
+            <div className='text-center' style={{color: 'white', padding: '50px 50px'}}>
+                <h2>JANUARY 17, 2021</h2>
+                <h4>SAN ANTONIO, TEXAS</h4>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
