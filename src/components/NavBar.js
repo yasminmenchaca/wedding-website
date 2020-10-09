4d7316679547d6bd2ade94c350c8dc4a5ae4a43c
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-10-09 18:45:39 -0500

    moved links to right

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,40 +4,39 @@ import * as ReactBootStrap from "react-bootstrap";
 
 const NavBar = () => {
     return (
-        <div className="App">
-            <ReactBootStrap.Navbar collapseOnSelect expand="xl" bg="danger" variant="dark">
-                <ReactBootStrap.Navbar.Brand href="/">Lynn & Yasmin</ReactBootStrap.Navbar.Brand>
-                <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-                    <ReactBootStrap.Nav className="mr-auto">
-                        <Link to="/">
-                            <ReactBootStrap.Nav.Link href="/">Home</ReactBootStrap.Nav.Link>
-                        </Link>
+        <ReactBootStrap.Navbar collapseOnSelect expand="xl" bg="danger" variant="dark" fixed="top">
+            <ReactBootStrap.Navbar.Brand href="/">Lynn & Yasmin</ReactBootStrap.Navbar.Brand>
+            <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+            <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+                <ReactBootStrap.Nav className="mr-auto">
+                </ReactBootStrap.Nav>
+                <ReactBootStrap.Nav>
+                    <Link to="/">
+                        <ReactBootStrap.Nav.Link href="/">Home</ReactBootStrap.Nav.Link>
+                    </Link>
 
-                        <Link to="/registry">
-                            <ReactBootStrap.Nav.Link href="/registry">Registry</ReactBootStrap.Nav.Link>
-                        </Link>
+                    <Link to="/registry">
+                        <ReactBootStrap.Nav.Link href="/registry">Registry</ReactBootStrap.Nav.Link>
+                    </Link>
 
-                        <Link to="/schedule">
-                            <ReactBootStrap.Nav.Link href="/schedule">Schedule</ReactBootStrap.Nav.Link>
-                        </Link>
+                    <Link to="/schedule">
+                        <ReactBootStrap.Nav.Link href="/schedule">Schedule</ReactBootStrap.Nav.Link>
+                    </Link>
 
-                        <Link to="/rsvp">
-                            <ReactBootStrap.Nav.Link href="/rsvp">RSVP</ReactBootStrap.Nav.Link>
-                        </Link>
+                    <Link to="/rsvp">
+                        <ReactBootStrap.Nav.Link href="/rsvp">RSVP</ReactBootStrap.Nav.Link>
+                    </Link>
 
-                        <Link to="/faqs">
-                            <ReactBootStrap.Nav.Link href="/faqs">FAQs</ReactBootStrap.Nav.Link>
-                        </Link>
+                    <Link to="/faqs">
+                        <ReactBootStrap.Nav.Link href="/faqs">FAQs</ReactBootStrap.Nav.Link>
+                    </Link>
 
-                        <Link to="/watch">
-                            <ReactBootStrap.Nav.Link href="/watch">Watch Online</ReactBootStrap.Nav.Link>
-                        </Link>
-
-                    </ReactBootStrap.Nav>
-                </ReactBootStrap.Navbar.Collapse>
-            </ReactBootStrap.Navbar>
-        </div>
+                    <Link to="/watch">
+                        <ReactBootStrap.Nav.Link href="/watch">Watch Online</ReactBootStrap.Nav.Link>
+                    </Link>
+                </ReactBootStrap.Nav>
+            </ReactBootStrap.Navbar.Collapse>
+        </ReactBootStrap.Navbar>
     )
 }
 
