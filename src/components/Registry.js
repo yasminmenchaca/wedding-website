06af4912eda333baafd6f9fc7d49b0891b0c48b0
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-10-09 10:32:36 -0500

    first commit

---
+++ src/components/Registry.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About US</h1>
+                <p>About US page body content</p>
+            </div>
+        );
+    }
+}
+
+export default About;
---
+++ src/components/Schedule.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Contact US</h1>
+                <p>Contact US page body content</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
