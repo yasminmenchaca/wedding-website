4c1f81cb8a9d4ebbb87c3511e52a553ddf8deff0
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-10-09 18:46:03 -0500

    embed google forms

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -1,13 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Rsvp extends Component {
-    render() {
-        return (
-            <div>
-                <h1>RSVP</h1>
-            </div>
-        );
-    }
+const Rsvp = () => {
+    return (
+        <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSekVYwSvcaDqne8DWcA6cBirkj2EHvCROGcssnnvrPKUvA9lA/viewform?embedded=true"
+            style={{backgroundColor: "white"}}
+            width="100%"
+            height="3000px"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0">
+            Loading...
+        </iframe>
+    );
 }
 
 export default Rsvp;
