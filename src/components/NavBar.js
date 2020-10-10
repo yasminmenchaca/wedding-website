3d39843dc2c68184d1957e3190aa670051bd87cf
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-10-09 22:54:24 -0500

    removed links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import * as ReactBootStrap from "react-bootstrap";
 
 const NavBar = () => {
@@ -11,29 +10,12 @@ const NavBar = () => {
                 <ReactBootStrap.Nav className="mr-auto">
                 </ReactBootStrap.Nav>
                 <ReactBootStrap.Nav>
-                    <Link to="/">
                         <ReactBootStrap.Nav.Link href="/">Home</ReactBootStrap.Nav.Link>
-                    </Link>
-
-                    <Link to="/registry">
                         <ReactBootStrap.Nav.Link href="/registry">Registry</ReactBootStrap.Nav.Link>
-                    </Link>
-
-                    <Link to="/schedule">
                         <ReactBootStrap.Nav.Link href="/schedule">Schedule</ReactBootStrap.Nav.Link>
-                    </Link>
-
-                    <Link to="/rsvp">
                         <ReactBootStrap.Nav.Link href="/rsvp">RSVP</ReactBootStrap.Nav.Link>
-                    </Link>
-
-                    <Link to="/faqs">
                         <ReactBootStrap.Nav.Link href="/faqs">FAQs</ReactBootStrap.Nav.Link>
-                    </Link>
-
-                    <Link to="/watch">
                         <ReactBootStrap.Nav.Link href="/watch">Watch Online</ReactBootStrap.Nav.Link>
-                    </Link>
                 </ReactBootStrap.Nav>
             </ReactBootStrap.Navbar.Collapse>
         </ReactBootStrap.Navbar>
