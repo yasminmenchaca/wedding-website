2cb683e0b374416ccfd65fa1e00a3268cafa1ec7
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-11-02 16:44:45 -0600

    changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,35 +4,34 @@ import * as ReactBootStrap from "react-bootstrap";
 
 const NavBar = () => {
     return (
-        <ReactBootStrap.Navbar collapseOnSelect expand="xl" variant="light" fixed="top" style={{backgroundColor: 'white'}}>
-            <ReactBootStrap.Navbar.Brand href="/" style={{fontWeight: 'bold'}}>Lynn & Yasmin</ReactBootStrap.Navbar.Brand>
+        <ReactBootStrap.Navbar collapseOnSelect expand="xl" variant="light" fixed="top"
+                               style={{backgroundColor: 'white'}}>
+            <ReactBootStrap.Navbar.Brand href="/" style={{fontWeight: 'bold'}}>Lynn &
+                Yasmin</ReactBootStrap.Navbar.Brand>
             <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav"/>
             <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
                 <ReactBootStrap.Nav className="mr-auto">
                 </ReactBootStrap.Nav>
                 <ReactBootStrap.Nav>
-                    <Link to="/" style={{ textDecoration: 'none' }}>
+                    <Link to="/" style={{textDecoration: 'none'}}>
                         <ReactBootStrap.Nav.Link href="/">Home</ReactBootStrap.Nav.Link>
                     </Link>
 
-                    {/*<Link to="/registry" style={{ textDecoration: 'none' }}>*/}
+                    {/*<Link to="/registry" style={{textDecoration: 'none'}}>*/}
                     {/*    <ReactBootStrap.Nav.Link href="/registry">Registry</ReactBootStrap.Nav.Link>*/}
                     {/*</Link>*/}
 
-                    <Link to="/schedule" style={{ textDecoration: 'none' }}>
+                    <Link to="/schedule" style={{textDecoration: 'none'}}>
                         <ReactBootStrap.Nav.Link href="/schedule">Schedule</ReactBootStrap.Nav.Link>
                     </Link>
 
-                    <Link to="/rsvp" style={{ textDecoration: 'none' }}>
+                    <Link to="/rsvp" style={{textDecoration: 'none'}}>
                         <ReactBootStrap.Nav.Link href="/rsvp">RSVP</ReactBootStrap.Nav.Link>
                     </Link>
 
-                    {/*<Link to="/faqs" style={{ textDecoration: 'none' }}>*/}
-                    {/*    <ReactBootStrap.Nav.Link href="/faqs">FAQs</ReactBootStrap.Nav.Link>*/}
-                    {/*</Link>*/}
-
                     <Link to="/watch" style={{ textDecoration: 'none' }}>
-                        <ReactBootStrap.Nav.Link href="/watch">Watch Online</ReactBootStrap.Nav.Link>
+                    <ReactBootStrap.Nav.Link href="/watch">Watch
+                        Online</ReactBootStrap.Nav.Link>
                     </Link>
 
                 </ReactBootStrap.Nav>
