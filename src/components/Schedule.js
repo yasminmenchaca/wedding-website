e77f4080812c61c47b0299304b75243c1978a97f
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-11-05 15:34:53 -0600

    styling changes

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -8,7 +8,8 @@ const Schedule = () => {
         <ReactBootStrap.Container fluid
                                   className='text-center'
                                   style={{backgroundColor: 'white', padding: '10px'}}>
-            <h1 style={{fontWeight: 'bold'}}>Schedule</h1> <hr style={{border: '1px solid black'}}/>
+            <h1 style={{fontWeight: 'bold'}}>Schedule</h1>
+            <hr style={{border: '1px solid black'}}/>
             <ReactBootStrap.Row>
                 <ReactBootStrap.Col sm={6} className='align-self-center'>
                     <ReactBootStrap.ResponsiveEmbed aspectRatio="16by9">
@@ -17,22 +18,23 @@ const Schedule = () => {
                                 allowFullScreen/>
                     </ReactBootStrap.ResponsiveEmbed>
                     <h2 style={{fontWeight: 'bold'}}>CEREMONY, DINNER, AND A MOVIE</h2><br/>
-                    <h6>SUNDAY, JANUARY 17, 2021</h6>
-                    <h6>2:00 PM - 6:00 PM CST</h6><br/><br/>
+                    <h5>SUNDAY, JANUARY 17, 2021</h5>
+                    <h5>2:00 PM - 6:00 PM CST</h5> <br className='d-md-none'/> <br className='d-md-none'/>
                 </ReactBootStrap.Col>
+
                 <ReactBootStrap.Col sm={6} className='align-self-center'>
                     <ReactBootStrap.Image src={couple} style={{height: '100px'}}/>
-                    <h5>Alamo Drafthouse Cinema Stone Oak <br/>
+                    <h5>Alamo Drafthouse - Stone Oak <br/>
                         22806 U.S. 281 <br/>
-                        San Antonio, TX 78258<br/><br/>
-                        Cocktail Attire: Wear cocktail dresses, dressy jumpsuits, and dark jackets and
-                        slacks. <br/><br/>
-                        Dinner and a viewing of "The Disaster Artist" will follow after the ceremony.</h5>
-                    <br/>
-                    <a href="https://goo.gl/maps/Z2Byumo8oXfZYqDj6" className="btn btn-dark btn-lg"
-                       role="button">Map</a>
-                    <br/>
+                        San Antonio, TX 78258
+                        <br/><br/>
+                        Dinner and a viewing of "The Disaster Artist" will follow after the ceremony. <br/><br/>
+                        <b>For everyone’s safety, guests will be required to wear masks at all times, except when eating
+                            or drinking. Alamo Drafthouse will also check the temperature of guests as they arrive. </b><br/><br/>
+                        <a href="https://drafthouse.com/san-antonio/covid-safety" className="btn btn-dark btn-lg"
+                           role="button" target="_blank">Venue Policy</a></h5>
                 </ReactBootStrap.Col>
+
             </ReactBootStrap.Row>
         </ReactBootStrap.Container>
     );
