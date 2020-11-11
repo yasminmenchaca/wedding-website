6008a1583e7acfbdf337e901a4d8a15da15d34a7
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-11-11 10:37:53 -0600

    https redirect

--- server.js
+++ server.js
@@ -1,36 +1,17 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-import sslRedirect from 'heroku-ssl-redirect';
-import express from 'express';
+const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// enable ssl redirect
-app.use(sslRedirect());
 
-app.get('/', (req, res) => {
-    res.send('hello world');
+
+app.get('*', function (req, res, next) {
+    if (req.header('x-forwarded-proto') !== 'https') {
+        res.redirect(`https://${req.header('host')}${req.url}`)
+    } else {
+        next();
+    }
 });
 
-app.listen(process.env.PORT || 5000);
+app.use(express.static('build'));
 
-// app.use(express.static(path.join(__dirname, 'build')));
-//
-// app.get('*', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
-
-// app.use((req, res, next) => {
-//     if (req.header('x-forwarded-proto') !== 'https') {
-//         res.redirect(`https://${req.header('host')}${req.url}`)
-//     } else {
-//         next();
-//     }
-// });
-
-// app.use(express.static('build'));
-
-// app.listen(PORT);
+app.listen(PORT);
