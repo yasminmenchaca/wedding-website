ae5d2337a15b98e3bee6c4d03438ab616c17e89f
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-11-11 10:12:46 -0600

    https redirect

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
+//
+// app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.use(express.static('build'));
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+        res.redirect(`https://${req.header('host')}${req.url}`)
+    } else {
+        next();
+    }
 });
 
 app.listen(PORT);
